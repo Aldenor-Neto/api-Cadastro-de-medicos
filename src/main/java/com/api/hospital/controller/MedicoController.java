6938b302a8403a8dc5c2542dcb7f3d6894668c43
Aldenor-Neto <aldenor05062010@gmail.com>
2023-06-26 15:10:51 -0300

    api apenas com hello world e imprinindo string no console

---
+++ src/main/java/com/api/hospital/controller/MedicoController.java
@@ -0,0 +1,17 @@
+package com.api.hospital.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medico")
+
+public class MedicoController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody String json) {
+        System.out.println(json);
+    }
+}
